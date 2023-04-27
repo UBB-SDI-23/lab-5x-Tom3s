c6a2efe5ec19a03696a1eeaffb274ed42796f2d5
915_Nagy_Matyas <tomesjerry3@gmail.com>
2023-04-27 03:50:16 +0300

    Update swagger.ts

--- backend/src/swagger.ts
+++ backend/src/swagger.ts
@@ -13,7 +13,7 @@ function initSwagger(app: Express, port: number) {
             title: 'API Documentation for Boxes app',
             description: 'Description',
         },
-        host: 'localhost:80',
+        host: '13.48.137.148:80',
         schemes: ['http'],
     };
 
