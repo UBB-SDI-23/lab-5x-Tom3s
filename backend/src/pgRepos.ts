4cb81a53956320f66e0d4865133a00685bfdd5bc
915_Nagy_Matyas <tomesjerry3@gmail.com>
2023-05-10 17:22:20 +0300

    Update pgRepos.ts

--- backend/src/pgRepos.ts
+++ backend/src/pgRepos.ts
@@ -412,7 +412,7 @@ class PGComboRepository {
 
     async getCount(): Promise<number> {
         const result = await this.client.query("SELECT value FROM cache_table WHERE key = 'combos_count';");
-        return result.rows[0].count;
+        return result.rows[0].value;
     }
 }
 
