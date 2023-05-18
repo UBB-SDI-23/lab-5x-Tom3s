e4f4851057a104f21545c386a122a5252fb0a671
915_Nagy_Matyas <tomesjerry3@gmail.com>
2023-05-18 15:43:55 +0300

    def pagelength can be changed

--- frontend/src/userSystem/profileEditor.tsx
+++ frontend/src/userSystem/profileEditor.tsx
@@ -28,6 +28,10 @@ class ProfileEditor extends ItemEditor {
         {
             name: "birthday",
             type: "date"
+        },
+        {
+            name: "pagelength",
+            type: "number"
         }
     ]
     itemType: string = "profile";
@@ -65,6 +69,39 @@ class ProfileEditor extends ItemEditor {
                 const newDate = oldDate.split("T")[0];
                 data.birthday = newDate;
                 this.setState({ tempItem: data });
+            });
+    }
+
+    handleSubmit(event: any) {
+        event.preventDefault();
+
+        // concatanate all inputs into one array
+        const allInputs = [...this.input1stRow, ...this.input2ndRow, ...this.input3rdRow];
+
+        const formData = new FormData(event.target);
+
+        var itemToSubmit: any = {};
+        allInputs.forEach((input: any) => {
+            itemToSubmit[input.name] = formData.get(input.name);
+        });
+
+        fetch(this.getSubmitUrl(), this.getSubmitOptions(itemToSubmit))
+            .then(async (response) => {
+                // console.log(response);
+                var toast = {
+                    type: "Success",
+                    message: await response.text(),
+                    duration: 5000,
+                    visible: true,
+                    id: this.state.toasts.length !== 0 ? (this.state.toasts.at(-1).id + 1) : 0
+                };
+                if (![200, 201].includes(response.status)) {
+                    toast.type = "Warning";
+                    toast.duration = 10000;
+                } else {
+                    localStorage.setItem("pagelength", itemToSubmit.pagelength);
+                }
+                this.setState({ toasts: [...this.state.toasts, toast] });
             });
     }
 }
